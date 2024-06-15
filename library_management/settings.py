3fddb5444414aaa3b361882f2ee7b64dae81d197
Antu Saha <antusaha970@gmail.com>
2024-06-15 22:11:44 +0600

    added host and csrf trusted origings

--- library_management/settings.py
+++ library_management/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://e-library-django.onrender.com']
 
 
 # Application definition
